1b377ccde5dc66875f67536295694aa24b2b41c7
Rahul Gangurde <109671480+RahulGangurde1698@users.noreply.github.com>
2022-09-01 23:29:40 +0530

    Delete src/pages directory

--- src/pages/Customers.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Customers=()=>{
-    return(
-        <h1>This is Customers page</h1>
-    )
-}
-export default Customers;
--- src/pages/Products.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Products=()=>{
-    return(
-        <h1>This is products page</h1>
-    )
-}
-export default Products;
